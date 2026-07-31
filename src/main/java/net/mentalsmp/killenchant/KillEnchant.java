7c164e1e82801ac901cda0351d8c47d582142918
Away <damianmischitz12@gmail.com>
2026-07-31 19:05:11 +0200

    Create KillEnchant.java

---
+++ src/main/java/net/mentalsmp/killenchant/KillEnchant.java
@@ -0,0 +1,26 @@
+package net.mentalsmp.killenchant;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KillEnchant extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(
+                new KillListener(),
+                this
+        );
+
+        getServer().getPluginManager().registerEvents(
+                new UniversalEnchantListener(this),
+                this
+        );
+
+        getLogger().info("KillEnchant wurde erfolgreich aktiviert!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("KillEnchant wurde deaktiviert.");
+    }
+}
